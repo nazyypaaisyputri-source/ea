eb9a7274e7986404ab20755bc35bddca9b4c69a6
nazyypaaisyputri-source <nazyypaaisyputri@gmail.com>
2026-03-31 23:02:10 +0700

    menambahkan fungsi tampil enu untuk menampilkan daftar pilihan

--- cekbilangan.cpp
+++ cekbilangan.cpp
@@ -5,6 +5,13 @@ using namespace std;
 int angka;
 int pilihan;
 
+void tampilMenu() {
+    cout << "\n--- MENU CEK BILANGAN ---" << endl;
+    cout << "1. Cek Bilangan Prima" << endl;
+    cout << "2. Cek Bilangan Fibonacci" << endl;
+    cout << "0. Keluar" << endl;
+    cout << "Pilih menu: ";
+}
 int main() {
     return 0;
 }
